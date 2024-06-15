4f57e24d600056feb73945412904a12c47b61df8
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-15 14:30:43 +0700

    added root router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,28 +26,35 @@ import EventsPage from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
+import Root from "./pages/Root";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <HomePage />,
-    },
-    {
-      path: "events",
-      element: <EventsPage />,
-    },
-    {
-      path: "events/:eventId",
-      element: <EventDetailPage />,
-    },
-    {
-      path: "events/new",
-      element: <NewEventPage />,
-    },
-    {
-      path: "events/:eventId/edit",
-      element: <EditEventPage />,
+      element: <Root />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "events",
+          element: <EventsPage />,
+        },
+        {
+          path: "events/:eventId",
+          element: <EventDetailPage />,
+        },
+        {
+          path: "events/new",
+          element: <NewEventPage />,
+        },
+        {
+          path: "events/:eventId/edit",
+          element: <EditEventPage />,
+        },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
