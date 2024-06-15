b16b8bca8b9527f34463c89d94237241b088d8de
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-15 14:41:23 +0700

    Got the Output the ID of the selected event on the EventDetailPage

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,3 +1,11 @@
-export default function EventDetailPage(){
-    return <h1>EventDetailPage</h1>
+import { useParams } from "react-router-dom";
+
+export default function EventDetailPage() {
+  const params = useParams();
+  return (
+    <>
+      <h1>EventDetailPage</h1>
+      <p>{params.eventId}</p>
+    </>
+  );
 }
