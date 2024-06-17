38778c5ac8bb2b7e04f018352dd8d651b6fb983b
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-17 22:08:01 +0700

    alternative error handling

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -15,7 +15,9 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw { message: "Could not fetch events." };
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    }); // shows an nearest errorElement set in router
   } else {
     return response;
   }
