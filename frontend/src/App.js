58857f94793e91f4f29dfe8ccf8d8bf08bee5cb3
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-17 21:17:58 +0700

    implementing loader to fetch data set on router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,6 +24,16 @@ function App() {
             {
               index: true,
               element: <EventsPage />,
+              loader: async () => { // runs the function on page load
+                const response = await fetch("http://localhost:8080/events");
+
+                if (!response.ok) {
+                  //...
+                } else {
+                  const resData = await response.json();
+                  return resData.events;
+                }
+              },
             },
             {
               path: ":eventId",
